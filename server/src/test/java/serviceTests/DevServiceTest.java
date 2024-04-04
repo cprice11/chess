bc80df9a260544052336023d175d7ff6ccb46ca2
cprice11 <krister.price+github@gmail.com>
2024-04-04 00:51:31 -0600

    Dev Service is passing its test

--- server/src/test/java/serviceTests/DevServiceTest.java
+++ server/src/test/java/serviceTests/DevServiceTest.java
@@ -1,10 +1,30 @@
 package serviceTests;
 
+import dataAccess.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import service.DevService;
 
-class DevServiceTest extends ServiceTest{
+class DevServiceTest extends ServiceVars {
+    private static final AuthDAO auth = new MemoryAuthDAO();
+    private static final GameDAO games = new MemoryGameDAO();
+    private static final UserDAO users = new MemoryUserDAO();
+    private static final DevService dev = new DevService(auth, games, users);
+    private static final MemoryDatabase db = new MemoryDatabase();
+
+    @BeforeEach
+    void buildDatabase() {
+        MemoryDatabase.setAuth(authData);
+        MemoryDatabase.setGames(gameData);
+        MemoryDatabase.setUsers(userData);
+    }
 
     @Test
-    void clearDatabase(){
+    void clearDatabase() {
+        dev.clearDatabase();
+        Assertions.assertTrue(MemoryDatabase.getAuth().isEmpty());
+        Assertions.assertTrue(MemoryDatabase.getGames().isEmpty());
+        Assertions.assertTrue(MemoryDatabase.getUsers().isEmpty());
     }
 }
---
+++ server/src/test/java/serviceTests/ServiceVars.java
@@ -0,0 +1,34 @@
+package serviceTests;
+
+import chess.ChessGame;
+import model.AuthData;
+import model.GameData;
+import model.GameSummary;
+import model.UserData;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class ServiceVars {
+    static final AuthData a0 = new AuthData("0hu2^7T@YXtJhDuQ2%dg", "myname123");
+    static final AuthData a1 = new AuthData("1hu2^7T@YXtJhDuQ2%dg", "antonius");
+    static final AuthData a2 = new AuthData("2hu2^7T@YXtJhDuQ2%dg", "death");
+
+    static final GameData g0 = new GameData(1, "myname123", "Antonius", "firstmatch", new ChessGame());
+    static final GameData g1 = new GameData(2, "antonius", "death", "chessgame", new ChessGame());
+    static final GameData g2 = new GameData(3, "death", "myname123", "chessgame", new ChessGame());
+
+    static final UserData u0 = new UserData("myname123", "p@ssword", "email@email.com");
+    static final UserData u1 = new UserData("antonius", "G00dK!ght", "email@email.com");
+    static final UserData u2 = new UserData("death", "humor0us", "email@email.com");
+
+    static final HashSet<AuthData> authData = new HashSet<>(Arrays.asList(a0, a1, a2));
+    static final HashSet<GameData> gameData = new HashSet<>(Arrays.asList(g0, g1, g2));
+    static final HashSet<UserData> userData = new HashSet<>(Arrays.asList(u0, u1, u2));
+
+    static final HashSet<GameSummary> gameSummaries = new HashSet<>(Arrays.asList(
+            new GameSummary(g0.gameID(), g0.whiteUsername(), g0.blackUsername(), g0.gameName()),
+            new GameSummary(g1.gameID(), g1.whiteUsername(), g1.blackUsername(), g1.gameName()),
+            new GameSummary(g2.gameID(), g2.whiteUsername(), g2.blackUsername(), g2.gameName())
+    ));
+}
