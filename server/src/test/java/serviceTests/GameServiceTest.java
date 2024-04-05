80a027b8c86d30c2420bf9a62422ca52d93a2803
cprice11 <krister.price+github@gmail.com>
2024-04-04 18:31:48 -0600

    Starting to set up GameServiceTest

--- server/src/test/java/serviceTests/GameServiceTest.java
+++ server/src/test/java/serviceTests/GameServiceTest.java
@@ -1,16 +1,39 @@
 package serviceTests;
 
-import org.junit.jupiter.api.Test;
+import dataAccess.MemoryAuthDAO;
+import dataAccess.MemoryDatabase;
+import org.junit.jupiter.api.*;
+import service.AuthService;
+import service.GameService;
+import service.UserService;
 
-class GameServiceTest {
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class GameServiceTest extends ServiceVars{
+    private static AuthService authService = new AuthService(auth);
+    private static UserService userService = new UserService(users, authService);
+    private static GameService gameService = new GameService();
+
+    @BeforeEach
+    void buildDatabase() {
+        MemoryDatabase.setAuth(authData);
+        MemoryDatabase.setGames(gameData);
+        MemoryDatabase.setUsers(userData);
+        auth = new MemoryAuthDAO();
+        authService = new AuthService(auth);
+        userService = new UserService(users, authService);
+    }
+
+    @Test
+    @Order(1)
+    void testCreateGame() {
+        gameService.createGame(goodCreateGameRequest)
+    }
+
     @Test
     void testJoinGame() {
 
     }
 
-    @Test
-    void testCreateGame() {
-    }
 
     @Test
     void testGetGames() {
