f650b52fe9c90b2eee370b84de3bb1bd43b5fb8b
cprice11 <krister.price+github@gmail.com>
2024-04-05 01:06:01 -0600

    odd error database is null

--- server/src/main/java/server/RegisterHandler.java
+++ server/src/main/java/server/RegisterHandler.java
@@ -1,9 +1,11 @@
 package server;
 
+import dataAccess.DataAccessException;
 import dataAccess.MemoryAuthDAO;
 import dataAccess.MemoryGameDAO;
 import dataAccess.MemoryUserDAO;
 import server.request.RegisterRequest;
+import server.result.RegisterResult;
 import server.result.Result;
 import service.DevService;
 import spark.Request;
@@ -11,12 +13,16 @@ import spark.Response;
 
 public class RegisterHandler extends Handler{
     public static String handleRequest(Request req, Response res) {
+        String json;
         try {
             RegisterRequest parsedRequest = serializer.fromJson(req.body(), RegisterRequest.class);
-            users.register(parsedRequest);
-            return serializer.toJson(new Result(200, ""));
-        } catch (Exception e) {
-            return serializer.toJson(new Result(501, "Error: " + e));
+            RegisterResult result = users.register(parsedRequest);
+            json = serializer.toJson(result);
+            setStatusAndBody(res, 200, json);
+        } catch (DataAccessException e) {
+            json = serializer.toJson(new Result(401, "Error: unauthorized"));
+            setStatusAndBody(res, 401, json);
         }
+        return json;
     }
 }
