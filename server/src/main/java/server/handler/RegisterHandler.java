669805ae6df4a746b95549378493ab4faba1a01c
cprice11 <krister.price+github@gmail.com>
2024-04-05 14:21:46 -0600

    no null passwords

--- server/src/main/java/server/handler/RegisterHandler.java
+++ server/src/main/java/server/handler/RegisterHandler.java
@@ -1,6 +1,7 @@
 package server.handler;
 
 import dataAccess.DataAccessException;
+import server.request.InvalidRequestException;
 import server.request.RegisterRequest;
 import server.result.RegisterResult;
 import service.AlreadyTakenException;
@@ -11,11 +12,13 @@ public class RegisterHandler extends Handler{
     public static String handleRequest(Request req, Response res) {
         try {
             RegisterRequest parsedRequest = serializer.fromJson(req.body(), RegisterRequest.class);
+            if (parsedRequest.password() == null) throw new InvalidRequestException("No password");
             RegisterResult result = users.register(parsedRequest);
             return success(res, serializer.toJson(result));
         } catch (AlreadyTakenException e) {
+            return alreadyTaken(res);
+        } catch (InvalidRequestException e) {
             return badRequest(res);
-//            return alreadyTaken(res);
         }
     }
 }
