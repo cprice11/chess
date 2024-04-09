70034a7ad0a58d3b8cafee9d9cff2b1b31f746fe
cprice11 <krister.price+github@gmail.com>
2024-04-08 19:19:31 -0600

    userDao tests passing

--- server/src/test/java/dataAccessTests/sqlDaoTests/sqlUserDaoTest.java
+++ server/src/test/java/dataAccessTests/sqlDaoTests/sqlUserDaoTest.java
@@ -1,8 +1,8 @@
 package dataAccessTests.sqlDaoTests;
 
-import dataAccess.DataAccessException;
-import dataAccess.DatabaseManager;
-import dataAccess.UserDao;
+import dataAccess.*;
+import dataAccess.sqlDao.SQLAuthDao;
+import dataAccess.sqlDao.SQLGameDao;
 import dataAccess.sqlDao.SQLUserDao;
 import model.UserData;
 import org.junit.jupiter.api.*;
@@ -13,13 +13,17 @@ import java.util.HashSet;
 @SuppressWarnings("unused")
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class sqlUserDaoTest extends sqlDataAccessVars {
-    UserDao userDAO = new SQLUserDao();
+    private static AuthDao authDao;
+    private static GameDao gamesDao;
+    private static UserDao userDao;
 
     @BeforeAll
     @Order(1)
     static void initialize() {
         try {
-            DatabaseManager.configureDatabase();
+            authDao = new SQLAuthDao();
+            gamesDao = new SQLGameDao();
+            userDao = new SQLUserDao();
             DatabaseManager.resetData();
         } catch (DataAccessException e) {
             Assertions.fail("Could not set up database: " + e.getMessage());
@@ -30,9 +34,9 @@ public class sqlUserDaoTest extends sqlDataAccessVars {
     void setup() {
         try {
             DatabaseManager.resetData();
-            userDAO.add(u0);
-            userDAO.add(u1);
-            userDAO.add(u2);
+            userDao.add(u0);
+            userDao.add(u1);
+            userDao.add(u2);
         } catch (Exception e) {
             Assertions.fail("Unable to setup database for tests. Exception: " + e.getMessage());
         }
@@ -42,10 +46,10 @@ public class sqlUserDaoTest extends sqlDataAccessVars {
     @Order(1)
     void getAll() {
         try {
-            Collection<UserData> allUsers = userDAO.getAll();
+            Collection<UserData> allUsers = userDao.getAll();
             Assertions.assertEquals(userData, allUsers);
             Assertions.assertDoesNotThrow(DatabaseManager::resetData);
-            allUsers = userDAO.getAll();
+            allUsers = userDao.getAll();
             Assertions.assertTrue(allUsers.isEmpty());
         } catch (Exception e) {
             Assertions.fail("Failed due to an unexpected exception: " + e.getMessage());
@@ -56,11 +60,11 @@ public class sqlUserDaoTest extends sqlDataAccessVars {
     @Order(2)
     void delete() {
         try {
-            userDAO.delete(u0);
+            userDao.delete(u0);
             HashSet<UserData> smallerSet = new HashSet<>();
             smallerSet.add(u1);
             smallerSet.add(u2);
-            Assertions.assertEquals(smallerSet, userDAO.getAll());
+            Assertions.assertEquals(smallerSet, userDao.getAll());
         } catch (Exception e) {
             Assertions.fail("Failed due to an unexpected exception: " + e.getMessage());
         }
@@ -70,8 +74,8 @@ public class sqlUserDaoTest extends sqlDataAccessVars {
     @Order(3)
     void deleteAll() {
         try {
-            userDAO.deleteAll();
-            Collection<UserData> allUsers = userDAO.getAll();
+            userDao.deleteAll();
+            Collection<UserData> allUsers = userDao.getAll();
             Assertions.assertTrue(allUsers.isEmpty());
         } catch (Exception e) {
             Assertions.fail("Failed due to an unexpected exception: " + e.getMessage());
@@ -87,8 +91,8 @@ public class sqlUserDaoTest extends sqlDataAccessVars {
             withNewGuy.add(newGuy);
             withNewGuy.add(u1);
             withNewGuy.add(u2);
-            Assertions.assertDoesNotThrow(() -> userDAO.update(u0, newGuy));
-            Assertions.assertEquals(withNewGuy, userDAO.getAll());
+            Assertions.assertDoesNotThrow(() -> userDao.update(u0, newGuy));
+            Assertions.assertEquals(withNewGuy, userDao.getAll());
         } catch (Exception e) {
             Assertions.fail("Failed due to an unexpected exception: " + e.getMessage());
         }
@@ -98,9 +102,9 @@ public class sqlUserDaoTest extends sqlDataAccessVars {
     @Order(5)
     void verify() {
         try {
-        userDAO.delete(u0);
-        Assertions.assertThrows(DataAccessException.class, () -> userDAO.verify(u0));
-        Assertions.assertDoesNotThrow(() -> userDAO.verify(u2));
+        userDao.delete(u0);
+        Assertions.assertThrows(DataAccessException.class, () -> userDao.verify(u0));
+        Assertions.assertDoesNotThrow(() -> userDao.verify(u2));
         } catch (Exception e) {
             Assertions.fail("Failed due to an unexpected exception: " + e.getMessage());
         }
@@ -110,11 +114,11 @@ public class sqlUserDaoTest extends sqlDataAccessVars {
     @Order(6)
     void add() {
         try {
-        userDAO.deleteAll();
-        Assertions.assertDoesNotThrow(() -> userDAO.add(u0));
+        userDao.deleteAll();
+        Assertions.assertDoesNotThrow(() -> userDao.add(u0));
         HashSet<UserData> justOne = new HashSet<>();
         justOne.add(u0);
-        Assertions.assertEquals(justOne, userDAO.getAll());
+        Assertions.assertEquals(justOne, userDao.getAll());
         } catch (Exception e) {
             Assertions.fail("Failed due to an unexpected exception: " + e.getMessage());
         }
@@ -123,15 +127,15 @@ public class sqlUserDaoTest extends sqlDataAccessVars {
     @Test
     @Order(7)
     void getUser() {
-        Assertions.assertDoesNotThrow(() -> Assertions.assertEquals(userDAO.getUser(u0.username()), u0), "Threw unexpected exception");
+        Assertions.assertDoesNotThrow(() -> Assertions.assertEquals(userDao.getUser(u0.username()), u0), "Threw unexpected exception");
     }
 
     @Test
     @Order(9)
     void editUserPassword() {
         try {
-            userDAO.editUserPassword(u0.username(), "betterP@ssword");
-            Assertions.assertEquals(userDAO.getUser(u0.username()).password(), "betterP@ssword");
+            userDao.editUserPassword(u0.username(), "betterP@ssword");
+            Assertions.assertEquals(userDao.getUser(u0.username()).password(), "betterP@ssword");
         } catch (Exception e) {
             Assertions.fail("Unable to setup database for tests. Exception: " + e.getMessage());
         }
@@ -141,8 +145,8 @@ public class sqlUserDaoTest extends sqlDataAccessVars {
     @Order(10)
     void editUserEmail() {
         try {
-            userDAO.editUserEmail(u0.username(), "workemail@company.com");
-            Assertions.assertEquals(userDAO.getUser(u0.username()).email(), "workemail@company.com");
+            userDao.editUserEmail(u0.username(), "workemail@company.com");
+            Assertions.assertEquals(userDao.getUser(u0.username()).email(), "workemail@company.com");
         } catch (Exception e) {
             Assertions.fail("Unable to setup database for tests. Exception: " + e.getMessage());
         }
