cf429dff9e39e484e13eb6726e0c6d6746c9c188
cprice11 <krister.price+github@gmail.com>
2024-04-11 00:54:10 -0600

    phase 6 starter code

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,12 @@
 package server;
 
+import org.eclipse.jetty.websocket.api.Session;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import server.handler.*;
 import spark.Spark;
 
+@WebSocket
 public class Server {
 
 
@@ -26,6 +30,7 @@ public class Server {
     }
 
     private void createRoutes() {
+        Spark.webSocket("/connect", Server.class);
         Spark.delete("/db", ClearHandler::handleRequest);
         Spark.post("/user", RegisterHandler::handleRequest);
         Spark.post("/session", LoginHandler::handleRequest);
@@ -33,5 +38,11 @@ public class Server {
         Spark.get("/game", ListGamesHandler::handleRequest);
         Spark.post("/game", CreateGameHandler::handleRequest);
         Spark.put("/game", JoinGameHandler::handleRequest);
+    }
+
+    @OnWebSocketMessage
+    public void onMessage(Session session, String message) throws Exception {
+        System.out.printf("Received: %s", message);
+        session.getRemote().sendString("WebSocket response: " + message);
     }
 }
