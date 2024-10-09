ab2aab6c5b5fc59cc70aeabc2e9947c866b909a7
cprice11 <krister.price+github@gmail.com>
2024-10-09 15:12:49 -0600

    Override and hashCode methods (position tests passing)

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -10,6 +12,19 @@ public class ChessPosition {
     int rank;
     int file;
     char fileChar;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessPosition that = (ChessPosition) o;
+        return rank == that.rank && file == that.file;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rank, file);
+    }
 
     public ChessPosition(int rank, int file) {
         this.rank = rank;
