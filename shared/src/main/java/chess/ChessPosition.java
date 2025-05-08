e1fb420cf0a1c24a41a1e413548d8cf831485edd
christian <krister.price+github@gmail.com>
2025-05-07 21:20:55 -0600

    Position hash and equals

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -10,6 +12,20 @@ public class ChessPosition {
     private final int rank;
     private final int file;
     private final char fileCharacter;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPosition that = (ChessPosition) o;
+        return getRank() == that.getRank() && getFile() == that.getFile();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRank(), getFile());
+    }
 
     public ChessPosition(int rank, int file) {
         this.rank = rank;
