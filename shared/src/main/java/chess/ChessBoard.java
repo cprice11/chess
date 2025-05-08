256ff88e0f86fd8e31dd9caecaf46cf51827dcea
christian <krister.price+github@gmail.com>
2025-05-07 21:25:19 -0600

    Move hash, equals, set/get piece, and FEN strings

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,10 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,6 +12,22 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+    private final HashMap<ChessPosition, ChessPiece> pieces = new HashMap<>();
+    public final int BOARD_SIZE = 8;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessBoard that = (ChessBoard) o;
+        return Objects.equals(pieces, that.pieces);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(pieces);
+    }
 
     public ChessBoard() {
         
@@ -19,7 +40,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        pieces.put(position, piece);
     }
 
     /**
@@ -30,7 +51,7 @@ public class ChessBoard {
      * position
      */
     public ChessPiece getPiece(ChessPosition position) {
-        throw new RuntimeException("Not implemented");
+        return pieces.get(position);
     }
 
     /**
@@ -38,6 +59,74 @@ public class ChessBoard {
      * (How the game of chess normally starts)
      */
     public void resetBoard() {
-        throw new RuntimeException("Not implemented");
+        pieces.clear();
+        addPiece(new ChessPosition(1, 1), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(1, 2), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(1, 3), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(1, 4), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(1, 5), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(1, 6), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(1, 7), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(1, 8), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.ROOK));
+
+        for (int i = 1; i <= BOARD_SIZE; i++) {
+            addPiece(new ChessPosition(2, i), new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN));
+            addPiece(new ChessPosition(7, i), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.PAWN));
+        }
+
+        addPiece(new ChessPosition(8, 1), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(8, 2), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(8, 3), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(8, 4), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(8, 5), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(8, 6), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(8, 7), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(8, 8), new ChessPiece(ChessGame.TeamColor.BLACK, ChessPiece.PieceType.ROOK));
+        System.out.println(getFEN());
+    }
+
+    public String getFEN() {
+        // Turn
+        StringBuilder FEN = new StringBuilder(getFENBoard()).append(" ");
+//        FEN.append((turn == ChessGame.TeamColor.WHITE) ? "w" : "b").append(" ");
+        // Castling
+        StringBuilder castling = new StringBuilder();
+//        castling.append(whiteCanCastleShort ? "K" : "");
+//        castling.append(whiteCanCastleLong ? "Q" : "");
+//        castling.append(blackCanCastleShort ? "k" : "");
+//        castling.append(blackCanCastleLong ? "q" : "");
+        if (castling.isEmpty()) castling.append("-");
+        FEN.append(castling).append(" ");
+        // enPassant available
+//        FEN.append((enPassantTarget == null) ? "-" : enPassantTarget).append(" ");
+        // "Clock"
+//        FEN.append(getHalfMoveClock()).append(" ");
+//        FEN.append(getFullMoveNumber());
+        return FEN.toString();
+    }
+
+    public String getFENBoard() {
+        // Join rows together with slashes
+        List<String> rows = new ArrayList<>(8);
+        for (int i = BOARD_SIZE; i > 0; i--) {
+            rows.add(getFENRow(i));
+        }
+        return String.join("/", rows);
+    }
+
+    public String getFENRow(int rank) {
+        int numBlanks = 0;
+        StringBuilder row = new StringBuilder();
+        for (int i = 1; i <= BOARD_SIZE; i++) {
+            ChessPiece piece = getPiece(new ChessPosition(rank, i));
+            if (piece == null) numBlanks++;
+            else {
+                if (numBlanks != 0) row.append(numBlanks);
+                numBlanks = 0;
+                row.append(piece.toString());
+            }
+        }
+        if (numBlanks != 0) row.append(numBlanks);
+        return row.toString();
     }
 }
