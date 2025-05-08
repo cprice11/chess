63d66627770924042f9361c4a58c8badb0a04d68
christian <krister.price+github@gmail.com>
2025-05-07 20:55:28 -0600

    Setting up with Rank and File instead of row and column. getRow returns getRank

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -7,23 +7,67 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessPosition {
+    private final int rank;
+    private final int file;
+    private final char fileCharacter;
 
-    public ChessPosition(int row, int col) {
+    public ChessPosition(int rank, int file) {
+        this.rank = rank;
+        this.file = file;
+        this.fileCharacter = switch (file) {
+            case 1 -> 'A';
+            case 2 -> 'B';
+            case 3 -> 'C';
+            case 4 -> 'D';
+            case 5 -> 'E';
+            case 6 -> 'F';
+            case 7 -> 'G';
+            case 8 -> 'H';
+            default -> '?';
+        };
     }
 
     /**
      * @return which row this position is in
-     * 1 codes for the bottom row
+     * 1 codes for the bottom row or first rank
      */
     public int getRow() {
-        throw new RuntimeException("Not implemented");
+        return getRank();
     }
 
     /**
      * @return which column this position is in
-     * 1 codes for the left row
+     * 1 codes for the left row or A file
      */
     public int getColumn() {
-        throw new RuntimeException("Not implemented");
+        return getFile();
+    }
+
+    /**
+     * @return which rank this position is in
+     * 1 codes for the first rank
+     */
+    public int getRank() {
+        return this.rank;
+    }
+
+    /**
+     * @return which file this position is in
+     * 1 codes for the A file
+     */
+    public int getFile() {
+        return this.file;
+    }
+
+    /**
+     * @return the letter of the position's file A-H
+     */
+    public char getFileCharacter() {
+        return this.fileCharacter;
+    }
+
+    @Override
+    public String toString() {
+        return fileCharacter + String.valueOf(rank);
     }
 }
