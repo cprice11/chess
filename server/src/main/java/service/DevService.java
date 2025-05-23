429d3cdbb3ff6040b6284873ee6bd638351d9596
christian <krister.price+github@gmail.com>
2025-05-23 17:32:57 -0600

    dev service for clear

--- server/src/main/java/service/DevService.java
+++ server/src/main/java/service/DevService.java
@@ -1,4 +1,22 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+
 public class DevService extends Service{
+    AuthDAO auth;
+    UserDAO user;
+    GameDAO game;
+    public DevService(AuthDAO auth, GameDAO game, UserDAO user) {
+        this.auth = auth;
+        this.game = game;
+        this.user = user;
+    }
+    public void clear() {
+        this.auth.clearAll();
+        this.game.clearAll();
+        this.user.clearAll();
+        System.out.println("Clearing");
+    }
 }
--- server/src/main/java/service/Service.java
+++ server/src/main/java/service/Service.java
@@ -1,4 +1,9 @@
 package service;
 
+import java.util.UUID;
+
 public class Service {
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
+    }
 }
