ed93bf46f432a684e8734fa24012bc79ff4d2661
christian <krister.price+github@gmail.com>
2025-05-24 22:50:56 -0600

    List games tests

--- server/src/test/java/service/ListGamesTests.java
+++ server/src/test/java/service/ListGamesTests.java
@@ -1,4 +1,33 @@
 package service;
 
+import dataModels.GameSummary;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class ListGamesTests extends UnitTests{
+    @Test
+    @Order(1)
+    @DisplayName("Positive list games")
+    public void normalListGames() {
+        Collection<GameSummary> gameSummaries = new ArrayList<>();
+        gameSummaries.add(new GameSummary(gameA.gameID(), gameA.blackUsername(), gameA.whiteUsername(), gameA.gameName()));
+        gameSummaries.add(new GameSummary(gameB.gameID(), gameB.blackUsername(), gameB.whiteUsername(), gameB.gameName()));
+        try {
+            Assertions.assertEquals(gameSummaries, game.listGames(authA.authToken()));
+        } catch (Exception e) {
+            Assertions.fail("Unexpected exception");
+        }
+    }
+    @Test
+    @Order(2)
+    @DisplayName("Unauthorized list games")
+    public void unauthorizedListGames() {
+        Assertions.assertThrows(UnauthorizedException.class, () -> game.listGames(authC.authToken()));
+    }
 }
