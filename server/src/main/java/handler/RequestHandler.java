95d1085554079cc7b1ea389ec605144e7c27adca
christian <krister.price+github@gmail.com>
2025-06-06 15:47:42 -0600

    using Memory DAOs

--- server/src/main/java/handler/RequestHandler.java
+++ server/src/main/java/handler/RequestHandler.java
@@ -10,14 +10,16 @@ import spark.Response;
 import spark.Route;
 
 public abstract class RequestHandler implements Route {
-    protected static final AuthDAO AUTH_DAO = new MySqlAuth();
-    protected static final GameDAO GAME_DAO = new MySqlGame();
-    protected static final UserDAO USER_DAO = new MySqlUser();
+    protected static final AuthDAO AUTH_DAO = new MemoryAuth();
+    protected static final GameDAO GAME_DAO = new MemoryGame();
+    protected static final UserDAO USER_DAO = new MemoryUser();
     protected static final UserService USER_SERVICE = new UserService(AUTH_DAO, USER_DAO);
     protected static final GameService GAME_SERVICE = new GameService(AUTH_DAO, GAME_DAO);
     protected static final DevService DEV_SERVICE = new DevService(AUTH_DAO, GAME_DAO, USER_DAO);
     protected static final Gson GSON = new Gson();
-    private record Message(String message){};
+
+    private record Message(String message) {
+    }
     abstract public Object handle(Request request, Response response);
 
     public Object error(Response response, int code, String message) {
