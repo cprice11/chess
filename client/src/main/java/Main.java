638f863de375f0d92a79c38da1da03a6419de498
christian <krister.price+github@gmail.com>
2025-06-08 03:49:58 -0600

    code quality use methods

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,10 +1,26 @@
+import chess.ChessColor;
 import chess.ChessGame;
 import chess.ChessPiece;
+import ui.ConsolePrinter;
+import ui.PieceCharacters;
 import ui.Repl;
+
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        ChessGame game = new ChessGame();
+        ConsolePrinter printer = new ConsolePrinter(game);
+        ChessColor color = new ChessColor();
+        ChessColor.ColorPalette palette = new ChessColor.ColorPalette(Color.decode("#FFFFFF"), Color.decode("#000000"),
+                Color.decode("#302E2B"), Color.decode("#F9F9F9"), Color.decode("#343534"),
+                Color.decode("#d8daa4"), Color.decode("#739552"), Color.decode("#F5F580"),
+                Color.decode("#B9CA42"), Color.decode("#82CBBA"), Color.decode("#46A07C"),
+                Color.decode("#4EB7B4"), Color.decode("#302D29"), Color.decode("#F73E2C"),
+                Color.decode("#F73E2C"));
+        printer.setTheme(palette);
+        printer.setPieceCharacters(PieceCharacters.LETTER_SYMBOLS);
         System.out.println("Starting ♕ 240 Chess Client " + piece.prettyString() + "...");
         var serverUrl = "http://localhost:8765";
         if (args.length == 1) {
--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,10 +1,14 @@
-import chess.*;
+import chess.ChessColor;
+import chess.ChessGame;
+import chess.ChessPiece;
 import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        ChessColor useAllColors = new ChessColor();
+        useAllColors.primaryText().noBackground();
+        System.out.println(useAllColors + "♕ 240 Chess Server: " + piece + useAllColors.getResetString());
         Server server = new Server();
         server.run(8765);
     }
